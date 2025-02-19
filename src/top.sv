acc0cb1c472846e97e2e2010dad4d1b1fc8e6fd6
Danil Karpenko <limpix31@gmail.com>
2025-02-19 10:40:25 +0100

    chore: init

---
+++ src/top.sv
@@ -0,0 +1,52 @@
+module top (
+    input logic ref_clk,
+    input logic rst_n,
+
+    output logic [2:0] twi_mux,
+
+    output logic dvp_scl,
+    inout  logic dvp_sda,
+    input  logic dvp_vsync,
+    input  logic dvp_de,
+    input  logic dvp_pixel_clk,
+    output logic dvp_ref_clk,
+    output logic dvp_rst_n,
+    output logic dvp_power_down,
+
+    input logic [7:0] dvp_data,
+
+    output logic tmds_clk_p,
+    output logic tmds_clk_n,
+
+    output logic [2:0] tmds_chan_p,
+    output logic [2:0] tmds_chan_n,
+
+    output logic led_dvp_ready
+);
+
+    assign twi_mux = 3'b101;
+    assign dvp_power_down = 1'b0;
+    assign dvp_rst_n = 1'b1;
+//    assign dvp_ref_clk = ref_clk;
+
+    sys_pll u_sys_pll (
+        .ref_clk(ref_clk),
+        .rst_n(rst_n),
+        .dvp_clk(dvp_ref_clk),
+        .dvp_clk_en(1'b1)
+    );
+
+    logic dvp_ready;
+
+    assign led_dvp_ready = dvp_ready;
+
+    dvp_init u_dvp_init (
+        .clk  (ref_clk),
+        .rst_n(rst_n),
+        .scl  (dvp_scl),
+        .sda  (dvp_sda),
+        .done (dvp_ready)
+    );
+
+endmodule : top
+
---
+++ timings.sdc
@@ -0,0 +1,6 @@
+//Copyright (C)2014-2025 GOWIN Semiconductor Corporation.
+//All rights reserved.
+//File Title: Timing Constraints file
+//Tool Version: V1.9.11 
+//Created Time: 2025-02-23 14:23:13
+create_clock -name ref_clk -period 20 -waveform {0 10} [get_ports {ref_clk}]
